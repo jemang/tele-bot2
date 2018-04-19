7d9a65aa777f3e273f4ee05cfb987fb3411333c1
jemang <jempalo94@gmail.com>
2018-04-19 17:37:13 +0800

    update msj bot

--- msj.rb
+++ msj.rb
@@ -5,7 +5,7 @@ token = '555210897:AAGW4L1TyDQYZfDpzzLoLC0ILvQhzmhx154'
 chat = []
 Telegram::Bot::Client.run(token) do |bot|
 	myip = %x[curl http://ip.42.pl/raw]
-	msg = 'Your server is rebooting.\nNew ip address is :' + myip + '\n'
+	msg = "Your server is rebooting.\nNew ip address is :" + myip + "\n"
 	
 	# chat_id = 127839460
 	chat = ['163009424', '127839460']
